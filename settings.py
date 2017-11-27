d15f9bd67336bc407fa736eabd70179aa3151168
achicha <meth787@gmail.com>
2017-11-27 13:51:39 +0300

    fix: abspath for database

--- settings.py
+++ settings.py
@@ -7,6 +7,6 @@ path = os.path.abspath(os.path.dirname(sys.argv[0]))
 if os.path.isfile(os.path.join(path, '.env')):
     env.read_envfile(os.path.join(path, '.env'))
 
-DATABASE_URL = env.str('DATABASE_URL')
+DATABASE_URL = env.str('DATABASE_CON') + os.path.join(path, env.str('DATABASE_NAME'))
 HEROKU_URL = env.str('HEROKU_URL')
 URL_SUFFIX = env.str('URL_SUFFIX')
