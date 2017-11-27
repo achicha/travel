a31fe4147415411ab8dc7fe01f4d7bbd773fe9ba
achicha <meth787@gmail.com>
2017-11-27 13:43:29 +0300

    fix: abspath for .env part2 :)

--- settings.py
+++ settings.py
@@ -2,8 +2,10 @@ import os
 import sys
 from envparse import env
 
-if os.path.isfile('.env'):
-    env.read_envfile(os.path.join(os.path.abspath(os.path.dirname(sys.argv[0])), '.env'))
+path = os.path.abspath(os.path.dirname(sys.argv[0]))
+
+if os.path.isfile(os.path.join(path, '.env')):
+    env.read_envfile(os.path.join(path, '.env'))
 
 DATABASE_URL = env.str('DATABASE_URL')
 HEROKU_URL = env.str('HEROKU_URL')
