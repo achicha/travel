14decc4fa76edc85e1a620f53093c7b2c0f541a5
achicha <meth787@gmail.com>
2017-11-30 15:29:09 +0300

    fix: similar naming

--- run.py
+++ run.py
@@ -14,9 +14,9 @@ from settings import DATABASE_URL, HEROKU_URL, URL_SUFFIX
 @click.argument('hometown')
 @click.option('--debug', '-d', type=bool, is_flag=True, help='debug mode on, for testing purpose only ')
 @click.option('--init', '-i', type=bool, is_flag=True, help='init config, write data from config to DB ')
-@click.option('--airports', '-a', type=bool, is_flag=True, help='add all airports to DB')
-@click.option('--city', '-c', type=bool, is_flag=True, help='add all destinations (city) to DB')
-def cli(hometown, debug, init, airports, city):
+@click.option('--air', '-a', type=bool, is_flag=True, help='add all airports to DB')
+@click.option('--flight', '-f', type=bool, is_flag=True, help='add all destinations (flight end point) to DB')
+def cli(hometown, debug, init, air, flight):
     click.echo('start')
     if debug:
         lvl = 'DEBUG'
@@ -44,13 +44,13 @@ def cli(hometown, debug, init, airports, city):
     if init:
         tickets_db.create_db()
     # insert new found airports
-    if airports:
+    if air:
         found_airports = airports()
         tickets_db.add_new_airport(found_airports)
         logger.info('total found_airports: {}'.format(len(found_airports)))
         exit(0)
     # insert new found destinations
-    if city:
+    if flight:
         found_destinations = destinations(hometown)
         tickets_db.add_new_destination(hometown, found_destinations)
         logger.info('total found_destinations: {}'.format(len(found_destinations)))
