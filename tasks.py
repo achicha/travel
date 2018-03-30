61d8d8a8e2af8bab3a13403fc9a36f3da7372bea
achicha <meth787@gmail.com>
2018-03-30 13:22:24 +0300

    fix: celery task should return json serializable value

--- tasks.py
+++ tasks.py
@@ -10,10 +10,14 @@ path = os.path.abspath(os.path.dirname(__file__))
 
 @celery.task
 def aviasales_parser(from_='LWN', to='MOW',
-                     start='2018-04-28', end='2018-05-03', price='4000'):
+                     start='2018-04-28', end='2018-05-03', price='10000'):
 
     command = 'python ' + os.path.join(path, 'run.py') + \
               ' aviasales -from {} -to {} -s {} -e {} -p {}'.format(from_, to, start, end, price)
     #r = os.system(command)
     result = subprocess.check_output(command.split(), stderr=subprocess.STDOUT)
-    return result
+    return result.decode()
+
+
+if __name__ == '__main__':
+    aviasales_parser()
