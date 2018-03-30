2d4c0a47f75b2c4620bfc0c170571c7ef24a7692
achicha <meth787@gmail.com>
2018-03-30 14:31:55 +0300

    fix: crontab setup

--- celeryconfig.py
+++ celeryconfig.py
@@ -5,7 +5,7 @@ from settings import BROKER_URL, CELERY_RESULT_BACKEND
 CELERYBEAT_SCHEDULE = {
     'every-minute': {
         'task': 'tasks.aviasales_parser',
-        'schedule': crontab(minute='*/15', hour='*,8-22'),  # every 15 minute in working hours
+        'schedule': crontab(minute='*/15', hour='8-22'),  # every 15 minute in working hours
         'args': ('LWN', 'MOW', '2018-04-28', '2018-05-03', '4000')
     }
 }
